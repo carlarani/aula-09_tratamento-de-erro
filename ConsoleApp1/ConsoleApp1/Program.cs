34b5e99c043881e4d6e25a5d0bf511ea4dc332ba
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-13 15:46:34 -0300

    adicionei o projeto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+    double resultado = 0;
+
+    double somarValor(doble valor) { }
+
+    double subtrairValor(doble valor) { }
+
+    double dividirValor(doble valor) { }
+
+    double multiplicarValor(doble valor) { }
+
+    double zerarResultado() { }
+
+    double pegarResultadoDaUltimaOperacaoMatematica() { }
+
+    double definirResultadoDaUltimaOperacaoMatematica() { }
+
